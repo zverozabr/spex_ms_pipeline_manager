1745117d69eaefacd03eb22242ac25042bb881a1
Artem Zubkov <artzub@gmail.com>
2021-08-07 12:36:36 +0700

    added if parent=100, task status for childs,(boxes=> tasks is 0)

--- app.py
+++ app.py
@@ -6,24 +6,41 @@ import os
 from services.Timer import every
 from services.Task import task
 
-
 collection = 'pipeline'
 
 
-def update_box_status(data, status, session, project):
+def update_tasks_to_start_from_pending(data, status, _session):
+    if status < 100:
+        for box in data['boxes']:
+            for _task in box['tasks']:
+                if _task['status'] < 100 and _task['status'] != -1:
+                    resp = _session.put(url=f'{base_url}tasks/{_task.get("id")}', json={'status': -1})
+                    if resp.status_code != 200:
+                        print(f'error on update task: {task}')
+    if status == 100:
+        for box in data['boxes']:
+            for _task in box['tasks']:
+                if _task['status'] == -1:
+                    resp = _session.put(url=f'{base_url}tasks/{_task.get("id")}', json={'status': 0})
+                    if resp.status_code != 200:
+                        print(f'error on update task: {task}')
+
+
+def update_box_status(data, status, _session, project):
+    update_tasks_to_start_from_pending(data, status, _session)
     if data.get('status') != int(round(status, 0)):
-        resp = session.put(url=f'{base_url}pipeline/update/{project}/{data.get("id")}', json={'complete': int(round(status, 0))})
+        resp = _session.put(url=f'{base_url}pipeline/update/{project}/{data.get("id")}', json={'complete': int(round(status, 0))})
         if resp.status_code != 200:
             print(f'error on update: {data}')
 
 
-def recursion(data, session, project):
+def recursion(data, _session, project):
     if tasks := data.get('tasks'):
         box_status = 0 if sum(item['status'] for item in tasks) / len(tasks) < 0 else sum(item['status'] for item in tasks) / len(tasks)
-        update_box_status(data, box_status, session, project)
+        update_box_status(data, box_status, _session, project)
     if len(data['boxes']) > 0:
         for item in data['boxes']:
-            recursion(item, session, project)
+            recursion(item, _session, project)
 
 
 def get_box():
@@ -37,14 +54,12 @@ def get_box():
             if line_resp.status_code == 200:
                 data = line_resp.json()
                 if data["success"] is True:
-                    recursion(data["data"]["pipelines"][0], session=ses, project=line["project"])
+                    recursion(data["data"]["pipelines"][0], _session=ses, project=line["project"])
     print(resp, lines)
 
 
 if __name__ == '__main__':
     load_config()
     base_url = os.getenv("BACKEND_URL")
-    get_box()
-    # insert_task_result()
-    # every(1, get_sub_tasks)
-    # get_sub_tasks()
+
+    every(10, get_box)
