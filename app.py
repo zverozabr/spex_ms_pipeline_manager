17bdda869177623a7509d58d0a86fbdaf9649274
Artem Zubkov <artzub@gmail.com>
2022-05-26 01:56:43 +0700

    removed task updated. change logic of setting status for job.

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from spex_common.modules.database import db_instance
 from spex_common.services.Timer import every
 import spex_common.services.Pipeline as PipelineService
 import spex_common.services.Job as JobService
-import spex_common.services.Task as TaskService
 from spex_common.models.Status import PipelineStatus
 import logging
 
@@ -12,60 +11,30 @@ logger = get_logger("pipeline_manager")
 collection = "pipeline"
 
 
-def update_tasks_to_start_from_pending(data, status, first):
-    if status < 100:
-        for box in data.get("jobs", []):
-            for _task in box.get("tasks", []):
-                if 0 <= _task["status"] < 100:
-                    _data = {"status": -1}
-                    logger.debug(f"task {_task.get('id')} updating {_data}")
-                    TaskService.update(_task.get("id"), data=_data)
-
-    if status == 100:
-        for box in data.get("jobs", []):
-            for _task in box.get("tasks", []):
-                if _task["status"] == -1:
-                    _data = {"status": 0}
-                    logger.debug(f"task {_task.get('id')} updating {_data}")
-                    TaskService.update(_task.get("id"), data=_data)
-
-    if first:
-        for _task in data["tasks"]:
-            if _task["status"] == -1:
-                _data = {"status": 0}
-                logger.debug(f"task {_task.get('id')} updating {_data}")
-                TaskService.update(_task.get("id"), data=_data)
-
-
-def update_box_status(data, status, first):
-    update_tasks_to_start_from_pending(data, status, first)
-
+def update_box_status(data, status):
     if data.get("status") == status:
         return
 
-    _data = {"status": status}
-    logger.debug(f"job {data.get('id')} updating {_data}")
-    JobService.update_job(data.get("id"), _data)
+    job_id = data.get("id")
+    data = {"status": status}
+    logger.debug(f"job {job_id} updating {data}")
+    JobService.update_job(job_id, data)
 
 
-def recursion(data, first=False, _status: int = None):
+def recursion(data, _status: int = None):
     if data is None:
         return
 
     tasks = data.get("tasks")
     if tasks is not None and len(tasks) > 0:
-        status = sum(item.get("status") for item in tasks) / len(tasks)
-        status = int(round(status, 0))
-        status = max(0, min(status, 100))
-        if _status:
-            status = _status
-        update_box_status(data, status, first)
+        status = min(item.get("status") for item in tasks if item.get("status") is not None)
+        update_box_status(data, status)
 
     if len(data.get("jobs", [])) < 1:
         return
 
     for item in data.get("jobs", []):
-        recursion(item, first=False, _status=_status)
+        recursion(item, _status=_status)
 
 
 def get_box():
@@ -79,7 +48,7 @@ def get_box():
             if data[0].get("status") in [PipelineStatus.stopped.value, PipelineStatus.started.value]:
                 status = data[0].get("status")
 
-            recursion(data[0], first=True, _status=status)
+            recursion(data[0], _status=status)
 
             job_ids = PipelineService.get_jobs(data[0].get('jobs', []))
             jobs = JobService.select_jobs(condition="in", _id=job_ids)
